b8fbd6524089f4ba206817d27b42341bb6659f38
theeshiikanS <s.theeshiikan@gmail.com>
2026-06-08 14:40:00 -0400

    Update data.js

--- data.js
+++ data.js
@@ -4,16 +4,16 @@
 var BALL_RUN_DATA = {
     players: [
         // Format: ["Name", "Tier/Ranking", "Record", "Height", "Weight", "Wingspan", "Photo URL"]
-        ["Calvin", "Carry+", "2-0", "6'1\"", "180 lbs", "6'3\"", ""],
-        ["Karnveer", "Mid", "2-0", "--", "--", "--", ""],
-        ["Theesiik", "Carry+", "0-1", "--", "--", "--", ""],
-        ["Earl", "Body-", "1-1", "--", "--", "--", ""],
-        ["Stefan", "Liability+", "0-0", "--", "--", "--", ""],
+        ["Calvin", "Carry+", "2-0", "6'1\"", "180 lbs", "6'3\"", "calvin.jpeg"],
+        ["Karnveer", "Mid", "2-0", "--", "--", "--", "karnveer.jpeg"],
+        ["Theesik", "Carry+", "0-1", "--", "--", "--", ""],
+        ["Earl", "Body-", "1-1", "--", "--", "--", "earl.jpeg"],
+        ["Stefan", "Liability+", "0-0", "--", "--", "--", "stefan.jpeg"],
         ["Thomas", "Cardio", "0-0", "5'2", "--", "--", "https://github.com/theeshiikanS/CNOSports/blob/6b37d8e960a4f21d2167594cb13f5d6cc39f8183/FBD49003-B77E-4985-AD8B-3CFBF8F287EC.jpeg"],
-        ["Anish", "Body", "0-0", "--", "--", "--", ""],
+        ["Anish", "Body", "0-0", "--", "--", "--", "chuckles.jpeg"],
         ["Mario", "yn-", "0-0", "--", "--", "--", ""],
         ["Krishaal", "mid", "0-0", "--", "--", "--", ""],
-        ["Gurjot", "Liability+", "0-1", "--", "--", "--", ""],
+        ["Gurjot", "Liability+", "0-1", "--", "--", "--", "gurjot.jpeg"],
         ["Ethan", "Mid", "0-1", "--", "--", "--", ""],
         ["Jason", "Carry-", "0-1", "--", "--", "--", "3397DB0B-8B6E-4AD7-8ED7-B70F0323BBD6.jpeg"]
     ],

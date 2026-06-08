13e744decf7716da45d2da5a8285863798a6418f
theeshiikanS <s.theeshiikan@gmail.com>
2026-06-08 11:57:42 -0400

    Add master team spreadsheet data

---
+++ data.js
@@ -0,0 +1,27 @@
+// ==========================================
+// MASTER TEAM SPREADSHEET DATA
+// ==========================================
+var BALL_RUN_DATA = {
+    players: [
+        // Format: ["Name", "Tier/Ranking", "Record", "Height", "Weight", "Wingspan", "Photo URL"]
+        ["Calvin", "Carry+", "2-0", "6'1\"", "180 lbs", "6'3\"", ""],
+        ["Karnveer", "Mid", "2-0", "--", "--", "--", ""],
+        ["Theesiik", "Carry+", "0-1", "--", "--", "--", ""],
+        ["Earl", "Body-", "1-1", "--", "--", "--", ""],
+        ["Stefan", "Liability+", "0-0", "--", "--", "--", ""],
+        ["Thomas", "Cardio", "0-0", "--", "--", "--", ""],
+        ["Anish", "Body", "0-0", "--", "--", "--", ""],
+        ["Mario", "yn-", "0-0", "--", "--", "--", ""],
+        ["Krishaal", "mid", "0-0", "--", "--", "--", ""],
+        ["Gurjot", "Liability+", "0-1", "--", "--", "--", ""],
+        ["Ethan", "Mid", "0-1", "--", "--", "--", ""],
+        ["Jason", "Carry-", "0-1", "--", "--", "--", ""]
+    ],
+    history: [
+        // Format: { desc: "What changed", date: "Date", author: "Who", reviewers: "Who reviewed" }
+        { desc: "Added Krishaal", date: "6/8/2026", author: "KM", reviewers: "CB" },
+        { desc: "Jason to Carry- & TN to Cardio", date: "6/6/2026", author: "TS", reviewers: "EDSG, TN" },
+        { desc: "Tier Change - Legend to yn", date: "6/6/2026", author: "KM", reviewers: "" },
+        { desc: "Initial release", date: "6/5/2026", author: "KM", reviewers: "CB, SR" }
+    ]
+};

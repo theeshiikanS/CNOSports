c7c77ab2152afff642544e17ac1730a97d33a0c6
theeshiikanS <s.theeshiikan@gmail.com>
2026-06-09 09:58:07 -0400

    Update data.js

--- data.js
+++ data.js
@@ -4,21 +4,22 @@
 var BALL_RUN_DATA = {
     players: [
         // Format: ["Name", "Tier/Ranking", "Record", "Height", "Weight", "Wingspan", "Photo URL"]
-        ["Calvin", "Carry+", "2-0", "6'1\"", "180 lbs", "6'3\"", "calvin.jpeg"],
-        ["Karnveer", "Mid", "2-0", "--", "--", "--", "karnveer.jpeg"],
-        ["Theesik", "Carry+", "0-1", "--", "--", "--", ""],
-        ["Earl", "Body-", "1-1", "--", "--", "--", "earl.jpeg"],
-        ["Stefan", "Liability+", "0-0", "--", "--", "--", "stefan.jpeg"],
-        ["Thomas", "Cardio", "0-0", "5'2", "--", "--", "thomas.jpeg"],
-        ["Anish", "Body", "0-0", "--", "--", "--", "chuckles.jpeg"],
+        ["Calvin", "Carry+", "3-1", "6'1\"", "180 lbs", "6'3\"", "calvin.jpeg"],
+        ["Karnveer", "Mid", "3-1", "--", "--", "--", "karnveer.jpeg"],
+        ["Theesik", "Carry+", "1-2", "--", "--", "--", ""],
+        ["Earl", "Body-", "2-2", "--", "--", "--", "earl.jpeg"],
+        ["Stefan", "Body", "1-0", "--", "--", "--", "stefan.jpeg"],
+        ["Thomas", "Body", "1-1", "5'2", "--", "--", "thomas.jpeg"],
+        ["Anish", "Chuckels", "0-0", "--", "--", "--", "chuckles.jpeg"],
         ["Mario", "yn-", "0-0", "--", "--", "--", ""],
         ["Krishaal", "mid", "0-0", "--", "--", "--", ""],
-        ["Gurjot", "Liability+", "0-1", "--", "--", "--", "gurjot.jpeg"],
-        ["Ethan", "Mid", "0-1", "--", "--", "--", ""],
+        ["Gurjot", "Liability+", "0-2", "--", "--", "--", "gurjot.jpeg"],
+        ["Ethan", "Mid", "1-2", "--", "--", "--", ""],
         ["Jason", "Carry-", "0-1", "--", "--", "--", "3397DB0B-8B6E-4AD7-8ED7-B70F0323BBD6.jpeg"]
     ],
     history: [
         // Format: { desc: "What changed", date: "Date", author: "Who", reviewers: "Who reviewed" }
+        { desc: "Removed Cardio and added BUMM, Updated rankings", date: "6/9/2026", author: "CB", reviewers: "TS, SR" },
         { desc: "Added Krishaal", date: "6/8/2026", author: "KM", reviewers: "CB" },
         { desc: "Jason to Carry- & TN to Cardio", date: "6/6/2026", author: "TS", reviewers: "EDSG, TN" },
         { desc: "Tier Change - Legend to yn", date: "6/6/2026", author: "KM", reviewers: "" },

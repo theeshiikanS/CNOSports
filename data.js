c7947f25f7ef64d243f0b541d9c179baa6cf68a4
theeshiikanS <s.theeshiikan@gmail.com>
2026-06-10 15:23:33 -0400

    Update data.js

--- data.js
+++ data.js
@@ -4,21 +4,24 @@
 var BALL_RUN_DATA = {
     players: [
         // Format: ["Name", "Tier/Ranking", "Record", "Height", "Weight", "Wingspan", "Photo URL"]
-        ["Calvin", "Carry+", "3-1", "6'1\"", "180 lbs", "6'3\"", "calvin.jpeg"],
-        ["Karnveer", "Mid", "3-1", "--", "--", "--", "karnveer.jpeg"],
-        ["Theesik", "Carry+", "1-2", "--", "--", "--", ""],
-        ["Earl", "Body-", "2-2", "--", "--", "--", "earl.jpeg"],
+        ["Calvin", "Carry", "4-2", "6'1\"", "180 lbs", "6'3\"", "calvin.jpeg"],
+        ["Karnveer", "Staple", "4-2", "--", "--", "--", "karnveer.jpeg"],
+        ["Theesik", "Carry", "2-4", "--", "--", "--", ""],
+        ["Earl", "Body-", "4-3", "--", "--", "--", "earl.jpeg"],
         ["Stefan", "Body", "1-0", "--", "--", "--", "stefan.jpeg"],
-        ["Thomas", "Body", "1-1", "5'2", "--", "--", "thomas.jpeg"],
-        ["Anish", "Chuckels", "0-0", "--", "--", "--", "chuckles.jpeg"],
-        ["Mario", "yn-", "0-0", "--", "--", "--", ""],
-        ["Krishaal", "mid", "0-0", "--", "--", "--", ""],
-        ["Gurjot", "Liability+", "0-2", "--", "--", "--", "gurjot.jpeg"],
-        ["Ethan", "Mid", "1-2", "--", "--", "--", ""],
-        ["Jason", "Carry-", "0-1", "--", "--", "--", "3397DB0B-8B6E-4AD7-8ED7-B70F0323BBD6.jpeg"]
+        ["Thomas", "Body-", "1-1", "5'2", "--", "--", "thomas.jpeg"],
+        ["Anish", "Chuckels", "0-1", "--", "--", "--", "chuckles.jpeg"],
+        ["Mario", "yn--", "1-1", "--", "--", "--", ""],
+        ["Krishaal", "staple-", "1-2", "--", "--", "--", ""],
+        ["Julian", "Carry+", "1-1", "--", "--", "--", ""],
+        ["Kheirthan", "Liability-", "0-2", "--", "--", "--", ""],
+        ["Gurjot", "Liability++", "1-3", "--", "--", "--", "gurjot.jpeg"],
+        ["Ethan", "Staple+", "1-2", "--", "--", "--", ""],
+        ["Jason", "Carry-", "1-1", "--", "--", "--", "3397DB0B-8B6E-4AD7-8ED7-B70F0323BBD6.jpeg"]
     ],
     history: [
         // Format: { desc: "What changed", date: "Date", author: "Who", reviewers: "Who reviewed" }
+        { desc: "Updated rankings and added Julian and KT", date: "6/10/2026", author: "TS", reviewers: "EDSG, SR" },
         { desc: "Removed Cardio and added BUMM, Updated rankings", date: "6/9/2026", author: "CB", reviewers: "TS, SR" },
         { desc: "Added Krishaal", date: "6/8/2026", author: "KM", reviewers: "CB" },
         { desc: "Jason to Carry- & TN to Cardio", date: "6/6/2026", author: "TS", reviewers: "EDSG, TN" },

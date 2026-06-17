91b8a20ffe38647dad70f0d38584201d14c22aff
theeshiikanS <s.theeshiikan@gmail.com>
2026-06-17 11:53:45 -0400

    Update data.js

--- data.js
+++ data.js
@@ -4,23 +4,27 @@
 var BALL_RUN_DATA = {
     players: [
         // Format: ["Name", "Tier/Ranking", "Record", "Height", "Weight", "Wingspan", "Photo URL"]
-        ["Calvin", "Carry", "4-2", "6'1\"", "180 lbs", "6'3\"", "calvin.jpeg"],
-        ["Karnveer", "Staple", "4-2", "--", "--", "--", "karnveer.jpeg"],
-        ["Theesik", "Carry", "2-4", "--", "--", "--", ""],
-        ["Earl", "Body-", "4-3", "--", "--", "--", "earl.jpeg"],
-        ["Stefan", "Body", "1-0", "--", "--", "--", "stefan.jpeg"],
-        ["Thomas", "Body-", "1-1", "5'2", "--", "--", "thomas.jpeg"],
-        ["Anish", "Chuckels", "0-1", "--", "--", "--", "chuckles.jpeg"],
-        ["Mario", "yn--", "1-1", "--", "--", "--", ""],
-        ["Krishaal", "staple-", "1-2", "--", "--", "--", ""],
+        ["Calvin", "Carry-", "6-3", "6'1"", "147 lbs", "6'3\"", "calvin.jpeg"],
+        ["Karnveer", "Staple+", "6-4", "6'1", "209", "--", "karnveer.jpeg"],
+        ["Theesik", "Carry-", "5-6", "6'3", "194", "--", ""],
+        ["Earl", "Body", "6-5", "6'0", "155", "--", "earl.jpeg"],
+        ["Stefan", "Body", "4-1", "5'11", "180", "--", "stefan.jpeg"],
+        ["Thomas", "Body", "3-4", "5'2", "171", "--", "thomas.jpeg"],
+        ["Anish", "Chuckly YN", "0-1", "--", "--", "--", "chuckles.jpeg"],
+        ["Mario", "yn---", "1-1", "--", "--", "--", ""],
+        ["Krishaal", "Staple-", "1-2", "--", "--", "--", ""],
         ["Julian", "Carry+", "1-1", "--", "--", "--", ""],
-        ["Kheirthan", "Liability-", "0-2", "--", "--", "--", ""],
-        ["Gurjot", "Liability++", "1-3", "--", "--", "--", "gurjot.jpeg"],
-        ["Ethan", "Staple+", "1-2", "--", "--", "--", ""],
-        ["Jason", "Carry-", "1-1", "--", "--", "--", "3397DB0B-8B6E-4AD7-8ED7-B70F0323BBD6.jpeg"]
+        ["Kheirthan", "Liability-", "0-2", "6'2", "205", "--", ""],
+        ["Gurjot", "Liability+++", "4-4", "6'1", "210", "--", "gurjot.jpeg"],
+        ["Ethan", "Staple++", "4-3", "6'3", "165", "--", ""],
+        ["Jason", "Carry--", "1-1", "5'11", "160", "--", "3397DB0B-8B6E-4AD7-8ED7-B70F0323BBD6.jpeg"]
+        ["Jewella", "Body+++", "2-2", "--", "--", "--", ""],
+        ["Andrew", "yn-", "2-3", "--", "--", "--", ""],
+        ["Rishi", "Staple", "2-3", "--", "--", "--", ""]
     ],
     history: [
         // Format: { desc: "What changed", date: "Date", author: "Who", reviewers: "Who reviewed" }
+        { desc: "Updated rankings from Ball run 15/6/26 and added new players", date: "6/17/2026", author: "TS", reviewers: "L3" },
         { desc: "Updated rankings and added Julian and KT", date: "6/10/2026", author: "TS", reviewers: "EDSG, SR" },
         { desc: "Removed Cardio and added BUMM, Updated rankings", date: "6/9/2026", author: "CB", reviewers: "TS, SR" },
         { desc: "Added Krishaal", date: "6/8/2026", author: "KM", reviewers: "CB" },
